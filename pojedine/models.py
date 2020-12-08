e7b5701ae16d2695fff61a480a291046cfd639b8
Tomislav Kralj <tomislak@gmail.com>
2020-12-08 13:40:06 +0100

    added first model

--- pojedine/models.py
+++ pojedine/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Pojedine(models.Model):
+    datum = models.DateField()
+    struja = models.DecimalField(max_digits=7, decimal_places=1)
+    plin = models.DecimalField(max_digits=7, decimal_places=3)
+    voda = models.DecimalField(max_digits=7, decimal_places=4)
+
+    def __str__(self):
+        return str(self.datum)
