badd5b0993ef14eac442c1aade459d1a3a988ec2
Tomislav Kralj <tomislak@gmail.com>
2020-12-08 14:42:08 +0100

    novi model baze, odvojeno

--- pojedine/models.py
+++ pojedine/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
-class Pojedine(models.Model):
+class Struja(models.Model):
     datum = models.DateField()
     struja = models.DecimalField(max_digits=7, decimal_places=1)
+
+    def __str__(self):
+        return str(self.datum) + " " + str(self.struja)
+
+class Plin(models.Model):
+    datum = models.DateField()
     plin = models.DecimalField(max_digits=7, decimal_places=3)
+
+    def __str__(self):
+        return str(self.datum)
+
+class Voda(models.Model):
+    datum = models.DateField()
     voda = models.DecimalField(max_digits=7, decimal_places=4)
 
     def __str__(self):
